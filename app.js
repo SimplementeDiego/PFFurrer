ce0493f072536e530bc18979a74444e2b5f0fd3e
dfurrer <diegodellepianefurrer@gmail.com>
2023-05-13 13:55:31 -0300

    Arreglos de legibilidad en JS

--- app.js
+++ app.js
@@ -1,36 +1,25 @@
+//Agarrar elementos del DOM
 const productsContainer = document.getElementById("products-container");
 const inputBuscar = document.getElementById("buscar");
 const sumaTotal = document.getElementById("resultado");
 const cantidadTotal = document.getElementById("cantidad");
 const numeroCarrito = document.getElementById("numeroCarrito");
 
+
+//Creo variables
+const apiUrl = "https://fakestoreapi.com/products";
+const products = [];
+const primeraMitad = [];
+const segundaMitad = [];
+let cart = {};
+
+
+//Acciones independientes
 inputBuscar.addEventListener("keydown", (event) => {
   if (event.keyCode === 13) {
     buscar();
   }
 });
-
-function sumaDecimal(...args) {
-  let res = 0;
-  if (args.length > 0) {
-    let arrDecimales = [];
-    args.forEach((element) => {
-      const decimales = (element.toString().split(".")[1] || "").length || 0;
-      arrDecimales.push(decimales);
-    });
-    const multiplicador = Math.pow(10, Math.max(...arrDecimales));
-    args.forEach((element) => {
-      res += element * multiplicador;
-    });
-    res = res / multiplicador;
-  }
-  return res.toFixed(2);
-}
-
-const apiUrl = "https://fakestoreapi.com/products";
-const products = [];
-const primeraMitad = [];
-const segundaMitad = [];
 
 fetch(apiUrl)
   .then((response) => response.json())
@@ -63,6 +52,25 @@ fetch(apiUrl)
   .catch((error) => {
     console.error("Error fetching data:", error);
   });
+
+
+//Funciones
+function sumaDecimal(...args) {
+  let res = 0;
+  if (args.length > 0) {
+    let arrDecimales = [];
+    args.forEach((element) => {
+      const decimales = (element.toString().split(".")[1] || "").length || 0;
+      arrDecimales.push(decimales);
+    });
+    const multiplicador = Math.pow(10, Math.max(...arrDecimales));
+    args.forEach((element) => {
+      res += element * multiplicador;
+    });
+    res = res / multiplicador;
+  }
+  return res.toFixed(2);
+}
 
 function buscar() {
   const desc = inputBuscar.value.trim().toLowerCase();
@@ -145,34 +153,6 @@ function sales() {
   });
 }
 
-let cart = {};
-
-function addToCart(item) {
-  if (cart[item.id]) {
-    cart[item.id].quantity++;
-  } else {
-    cart[item.id] = { ...item, quantity: 1 };
-  }
-
-  updateCart();
-  getTotalPrice();
-  saveCartToLocalStorage();
-}
-
-function removeFromCart(itemId) {
-  if (cart[itemId]) {
-    cart[itemId].quantity--;
-
-    if (cart[itemId].quantity === 0) {
-      delete cart[itemId];
-    }
-
-    updateCart();
-    getTotalPrice();
-    saveCartToLocalStorage();
-  }
-}
-
 function updateCart() {
   const cartList = document.getElementById("cart");
   cartList.innerHTML = "";
@@ -218,6 +198,34 @@ function getTotalPrice() {
   sumaTotal.innerHTML = totalPrice;
 }
 
+
+
+function addToCart(item) {
+  if (cart[item.id]) {
+    cart[item.id].quantity++;
+  } else {
+    cart[item.id] = { ...item, quantity: 1 };
+  }
+
+  updateCart();
+  getTotalPrice();
+  saveCartToLocalStorage();
+}
+
+function removeFromCart(itemId) {
+  if (cart[itemId]) {
+    cart[itemId].quantity--;
+
+    if (cart[itemId].quantity === 0) {
+      delete cart[itemId];
+    }
+
+    updateCart();
+    getTotalPrice();
+    saveCartToLocalStorage();
+  }
+}
+
 function toggleBox() {
   const boxContent = document.querySelector(".box-content");
   boxContent.style.display =
@@ -241,9 +249,8 @@ function restoreCartFromLocalStorage() {
 }
 
 function clearCart() {
-  cart = {}; // Vaciar el carrito
+  cart = {};
 
-  // Eliminar el carrito del local storage
   localStorage.removeItem("cart");
 
   updateCart();
@@ -252,41 +259,33 @@ function clearCart() {
 
 function confirmClearCart() {
   if (getCartItemCount() > 0) {
-    // Obtener el elemento del modal
     const modal = document.getElementById("confirmModal");
 
-    // Mostrar el modal
     modal.style.display = "block";
 
-    // Obtener los elementos de los botones de confirmación
     const confirmButton = document.getElementById("confirmButton");
     const cancelButton = document.getElementById("cancelButton");
 
-    // Agregar eventos de clic a los botones de confirmación
     confirmButton.addEventListener("click", handleConfirmClearCart);
     cancelButton.addEventListener("click", handleCancelClearCart);
 
-    // Función para manejar el clic en el botón de confirmación
     function handleConfirmClearCart() {
-      clearCart(); // Vaciar el carrito
-      closeModal(); // Cerrar el modal
+      clearCart();
+      closeModal();
     }
 
-    // Función para manejar el clic en el botón de cancelar
     function handleCancelClearCart() {
-      closeModal(); // Cerrar el modal
+      closeModal();
     }
 
-    // Función para cerrar el modal
     function closeModal() {
-      // Ocultar el modal
       modal.style.display = "none";
 
-      // Eliminar los eventos de clic de los botones de confirmación
       confirmButton.removeEventListener("click", handleConfirmClearCart);
       cancelButton.removeEventListener("click", handleCancelClearCart);
     }
   }
 }
 
+//Ejecutar al inicio
 restoreCartFromLocalStorage();
